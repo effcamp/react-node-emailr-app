1eedac88d1e2190074e67ca952075110af154189
effcamp <fcf.camposx@gmail.com>
2017-12-06 13:08:51 -0200

    Added billing and client side app

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const keys = require('./config/keys');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
+const bodyParser = require('body-parser');
 
 require('./models/User');
 require('./services/passport');
@@ -12,7 +13,7 @@ mongoose.connect(keys.mongoURI, { useMongoClient: true });
 mongoose.Promise = global.Promise;
 
 const app = express();
-
+app.use(bodyParser.json());
 app.use(
   cookieSession({
     maxAge: 30 * 24 * 60 * 60 * 1000,
@@ -24,9 +25,16 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 require('./routes/authRoute')(app);
-app.get('/', (req, res) => {
-  res.send('ok!');
-});
+require('./routes/billingRoutes')(app);
+
+// heroku production condition
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
